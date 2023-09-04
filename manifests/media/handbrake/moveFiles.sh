df3a31f99cd2f41ddc5c0fe9ba6b1b3831fb27cb
Nate Marshall <46655553+nate-marshall@users.noreply.github.com>
2023-09-04 18:05:39 -0500

    move script update

--- manifests/media/handbrake/moveFiles.sh
+++ manifests/media/handbrake/moveFiles.sh
@@ -3,15 +3,15 @@
 sabnzbd_complete_folder="/media/nate/Media12TB/media_downloads/downloads/complete/temptv"
 handbrake_watch_folder="/media/nate/Media12TB/media_downloads/tmp/tv"
 
-# Function to move files from source to destination
+# Function to move files from source to destination using rsync
 move_files() {
     source_folder="$1"
     destination_folder="$2"
-    for filename in "$source_folder"/*; do
-        [ -e "$filename" ] || continue  # Check if the file exists
-        destination_file="$destination_folder/$(basename "$filename")"
-        mv "$filename" "$destination_file"
-        echo "Moved: $(basename "$filename")"
+    for download_dir in "$source_folder"/*; do
+        [ -d "$download_dir" ] || continue  # Check if it's a directory
+        download_name="$(basename "$download_dir")"
+        rsync -a --remove-source-files "$download_dir/" "$destination_folder/$download_name/"
+        echo "Moved: $download_name"
     done
 }
 
