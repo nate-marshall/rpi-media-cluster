9dc9279d394d6ce4aa10b05bbdb03c998a75b26f
Nate Marshall <46655553+nate-marshall@users.noreply.github.com>
2023-09-24 11:49:32 -0500

    move script rewrite

--- manifests/media/handbrake/moveFiles.sh
+++ manifests/media/handbrake/moveFiles.sh
@@ -1,81 +1,86 @@
 #!/bin/bash
 
 # Set your folder paths
-sabnzbd_complete_folder="/media/nate/Media12TB/media_downloads/downloads/complete/temptv"
-handbrake_watch_folder="/media/nate/Media12TB/media_downloads/tmp/tv_from_sonarr"
-log_file="/media/nate/Media12TB/media_downloads/tmp/juggle.log"  # Specify the path to your log file
-max_retries=30  # Maximum number of retries
-retry_interval=120  # Sleep interval in seconds between each check
+source_folder="/media/nate/Media12TB/media_downloads/downloads/complete/temptv"
+temp_folder="/media/nate/Media12TB/media_downloads/tmp/tv_from_sonarr"
+log_file="/media/nate/Media12TB/media_downloads/tmp/juggle.log"
+max_retries=30                              # Maximum number of retries
+retry_interval=120                          # Sleep interval in seconds between each check
 
 # Function to log messages to a file
 log_message() {
     local message="$1"
-    echo "$(date +'%Y-%m-%d %H:%M:%S') - $message" | tee -a "$log_file"
+    local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+    echo "$timestamp - $message" | tee -a "$log_file"
 }
 
-# Function to move files from source to destination using rsync
-move_next_directory() {
-    source_folder="$1"
-    destination_folder="$2"
+# Function to move media files from source to temporary directory
+move_to_temp() {
+    local source="$1"
+    local temp="$2"
     
-    # Find the first available directory in the source folder
-    for download_dir in "$source_folder"/*; do
-        [ -d "$download_dir" ] || continue  # Check if it's a directory
-        download_name="$(basename "$download_dir")"
-
-        # Check if the folder starts with "_UNPACK_"
-        if [[ "$download_name" == "_UNPACK_"* ]]; then
-            log_message "Ignoring folder: $download_name"
-            continue
-        fi
-
-        # Move the directory to the destination
-        rsync -a --remove-source-files --exclude="_UNPACK_*" "$download_dir/" "$destination_folder/$download_name/" | tee -a "$log_file"
-        log_message "Moved: ${destination_folder}/${download_name}"
-        curl -X POST -H 'Content-Type: application/json' -d '{"text": "'"${destination_folder}/${download_name}"' download in progress..\n"}' "${CHAT_URL}"
-        sleep 5
-        return 0  # Return success
+    # Use find to search for .mp4 and .mkv files in the source directory
+    find "$source" -type f \( -iname "*.mp4" -o -iname "*.mkv" \) -print | while read -r media_file; do
+        local media_name="$(basename "$media_file")"
+        
+        # Move the media file to the temporary directory
+        mv "$media_file" "$temp/$media_name"
+        log_message "Moved to temp: $media_name"
     done
-
-    # No directories found in the source folder
-    return 1  # Return failure
 }
 
-# Function to cleanup the HandBrake watch folder
-cleanup_handbrake() {
-    rm -rf "${handbrake_watch_folder:?}/"*
-    log_message "Cleaned up HandBrake watch folder"
-}
-
-# Function to wait for HandBrake to finish transcoding
+# Function to wait for HandBrake to finish processing files
 wait_for_handbrake() {
     local retries=0
-    while [ "$retries" -lt "$max_retries" ]; do
-        if [ "$(ls -A "$handbrake_watch_folder")" ]; then
-            log_message "HandBrake is still transcoding ${destination_folder}/${download_name}. Retry $((retries+1)) of $max_retries..."
-            sleep "$retry_interval"
+    local files_processing=1
+    
+    while [ $retries -lt $max_retries ] && [ $files_processing -eq 1 ]; do
+        sleep $retry_interval
+        files_processing=0
+        
+        # Check if any files are still in the temporary directory (being processed by HandBrake)
+        if [ -n "$(ls -A "$temp_folder")" ]; then
+            log_message "HandBrake is still processing files:"
+            for processing_file in "$temp_folder"/*; do
+                log_message "Processing: $(basename "$processing_file")"
+            done
+            log_message "Retry $((retries+1)) of $max_retries..."
             ((retries++))
-        else
-            log_message "HandBrake has finished transcoding ${destination_folder}/${download_name}."
-            break
+            files_processing=1
         fi
     done
-    if [ "$retries" -eq "$max_retries" ]; then
+    
+    if [ $retries -eq $max_retries ]; then
         log_message "Max retries reached. Exiting with non-zero status."
-        curl -X POST -H 'Content-Type: application/json' -d '{"text": "'"${destination_folder}/${download_name}"' failed to transcode in time. Max retries reached."}' "${CHAT_URL}"
+        exit 1
+    else
+        log_message "HandBrake has finished processing files."
     fi
 }
 
-# Move files from SABnzbd to HandBrake one directory at a time
-log_message "Starting file transfer from SABnzbd to HandBrake. ${sabnzbd_complete_folder} -- ${handbrake_watch_folder}"
-while move_next_directory "$sabnzbd_complete_folder" "$handbrake_watch_folder"; do
-    :
-done
+# Move media files from source to temporary directory
+log_message "Starting file transfer to temporary directory"
+move_to_temp "$source_folder" "$temp_folder"
 
-# Wait for HandBrake to finish transcoding
-log_message "Waiting for HandBrake to finish transcoding. ${destination_folder}/${download_name}"
+# Check if there are no files to process
+if [ -z "$(ls -A "$temp_folder")" ]; then
+    log_message "No media files to process. Exiting."
+    exit 0  # Exit with success status
+fi
+
+# Wait for HandBrake to finish processing files
+log_message "Waiting for HandBrake to finish processing files"
 wait_for_handbrake
 
-# Cleanup the HandBrake watch folder
-# log_message "Starting cleanup of HandBrake watch folder"
-# cleanup_handbrake
+# Add your logic here to move files from the temporary directory to the final output directory
+# You can also log the names of files being processed in this section.
+
+# Example:
+# for processed_file in "$temp_folder"/*; do
+#     log_message "Transcoding: $(basename "$processed_file")"
+#     # Add your HandBrake transcoding command here
+#     # Move the transcoded file to the final output directory
+# done
+
+log_message "Files have been processed and moved to the final output directory."
+exit 0  # Exit with success status
